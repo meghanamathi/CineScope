86cdb987ae91ca132f887d87724473bb4a315b5f
meghanamathi <meghanamathi10@gmail.com>
2026-08-03 23:25:59 +0530

    Fix cors configuration

--- backend/server.js
+++ backend/server.js
@@ -12,7 +12,7 @@ app.use(
   cors({
     origin: [
       "http://localhost:3000",
-      "https://cine-sccope-meghana18.vercel.app",
+      "https://cine-scope-meghana18.vercel.app",
     ],
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     credentials: true,
